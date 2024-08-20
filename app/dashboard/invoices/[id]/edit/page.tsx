961062ab6fa8bc6acb66871a7d0c6e759370c122
Sebastian Londoño Diaz <stian0310@gmail.com>
2024-08-19 21:48:53 -0500

    add server actions for invoices: create, update and delete

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,32 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+
+interface PageProps {
+  params: {
+    id: string;
+  }
+}
+
+export default async function Page({ params }: PageProps) {
+  const { id } = params;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
